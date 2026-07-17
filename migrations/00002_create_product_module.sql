db930e5940440a8aed9b15611c52a504b82bfee3
harundarat <harundarat@gmail.com>
2026-07-17 09:24:06 +0700

    feat(migrations): seed initial data of products

--- migrations/00002_create_product_module.sql
+++ migrations/00002_create_product_module.sql
@@ -12,6 +12,14 @@ CREATE TABLE IF NOT EXISTS products (
   name VARCHAR(255) NOT NULL
 );
 
+INSERT INTO products (id, code, type, name) VALUES
+  ('00000000-0000-0000-0000-000000001001', 1001, 'mutual_fund', 'BNI-AM Indeks IDX30'),
+  ('00000000-0000-0000-0000-000000001002', 1002, 'mutual_fund', 'Schroder Dana Prestasi Plus'),
+  ('00000000-0000-0000-0000-000000002001', 2001, 'stock', 'Bank Central Asia Tbk'),
+  ('00000000-0000-0000-0000-000000002002', 2002, 'stock', 'Telkom Indonesia Tbk'),
+  ('00000000-0000-0000-0000-000000003001', 3001, 'bond', 'Obligasi Negara FR0100'),
+  ('00000000-0000-0000-0000-000000003002', 3002, 'bond', 'Obligasi Negara FR0103');
+
 CREATE TABLE IF NOT EXISTS product_prices (
   id UUID PRIMARY KEY,
   product_id UUID NOT NULL REFERENCES products(id),
